c65bea30b87ecb201cf1f663d3e3534817e25ba9
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2024-02-19 20:25:25 +1100

    Add `ex_doc`

--- impl/ex/mix.exs
+++ impl/ex/mix.exs
@@ -43,7 +43,8 @@ defmodule Pika.MixProject do
   defp deps do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:benchee, "~> 1.0", only: :dev}
+      {:benchee, "~> 1.0", only: :dev},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
