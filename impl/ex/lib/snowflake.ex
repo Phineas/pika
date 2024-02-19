d60c1fc975aa7cbf5b6cec3983de411993bbb65a
♥ ArtieFuzzz ♥ <hey@astolfo.cafe>
2024-02-19 20:48:09 +1100

    Fix Supervision of `Pika.Snowflake`

--- impl/ex/lib/snowflake.ex
+++ impl/ex/lib/snowflake.ex
@@ -41,7 +41,7 @@ defmodule Pika.Snowflake do
     GenServer.start_link(__MODULE__, {Utils.compute_node_id(), epoch, 0, 0}, name: __MODULE__)
   end
 
-  def start_link do
+  def start_link([]) do
     # State: {node_id, epoch, seq, last_sequence_exhaustion}
     GenServer.start_link(__MODULE__, {Utils.compute_node_id(), 1_640_995_200_000, 0, 0},
       name: __MODULE__
